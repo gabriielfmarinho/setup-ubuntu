f068c947b0295266c1cf20eab472171504c7034a
s2it_gmarinho <s2it_gmarinho@uolinc.com>
2020-03-15 16:40:27 -0300

    add new features

--- install.sh
+++ install.sh
@@ -10,18 +10,16 @@ install_spotify() {
 }
 
 install_chrome() {
-    local FILE_PATH=$HOME/Downloads/google-chrome.deb
-    wget -O $FILE_PATH https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg -i $FILE_PATH
-    rm $FILE_PATH
+    wget -O $HOME/Downloads/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i $HOME/Downloads/google-chrome.deb
+    rm $HOME/Downloads/google-chrome.deb
     sudo apt install -y -f
 }
 
 install_slack() {
-    local FILE_PATH=$HOME/Downloads/slack.deb
-    wget -O $FILE_PATH https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.1-amd64.deb
-    sudo dpkg -i $FILE_PATH 
-    rm $FILE_PATH
+    wget -O $HOME/Downloads/slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.1-amd64.deb
+    sudo dpkg -i $HOME/Downloads/slack.deb 
+    rm $HOME/Downloads/slack.deb
     sudo apt install -y -f
 }
 
@@ -31,10 +29,6 @@ install_gnome() {
 
 install_git() {
     cp ./configuration-files/gitconfig ~/.gitconfig
-}
-
-install_jdk11() {
-    sudo apt install openjdk-11-jdk
 }
 
 install_node() {
@@ -47,25 +41,10 @@ install_npm() {
 }
 
 install_vs_code() {
-    local FILE_PATH=$HOME/Downloads/vs-code.tar.gz
-    local DIR_WORKSPACE=$HOME/tools
-    wget -O $FILE_PATH https://go.microsoft.com/fwlink/?LinkID=620884
-    if [ ! -d $DIR_WORKSPACE ]; then
-            mkdir $DIR_WORKSPACE
-    fi 
-    tar -zxvf $FILE_PATH -C $DIR_WORKSPACE
-    rm $FILE_PATH
-}
-
-install_intellij() {
-    local FILE_PATH=$HOME/Downloads/idea.tar.gz
-    local DIR_WORKSPACE=$HOME/tools
-    wget -O $FILE_PATH https://download-cf.jetbrains.com/idea/ideaIC-2019.2.tar.gz
-    if [ ! -d $DIR_WORKSPACE ]; then
-            mkdir $DIR_WORKSPACE
-    fi 
-    tar -zxvf $FILE_PATH -C $DIR_WORKSPACE
-    rm $FILE_PATH
+    wget -O $HOME/Downloads/vs_code.deb https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable
+    sudo dpkg -i $HOME/Downloads/vs_code.deb
+    rm $HOME/Downloads/vs_code.deb
+    sudo apt install -y -f
 }
 
 install_docker() {
@@ -93,25 +72,40 @@ install_jdk8() {
     sudo apt install openjdk-8-jdk -y
 }
 
+install_jdk11() {
+    sudo apt install openjdk-11-jdk -y
+}
+
+install_jdk12() {
+    sudo apt install openjdk-12-jdk -y
+}
+
+install_jdk13() {
+    sudo apt install openjdk-13-jdk -y
+}
+
 install_postman() {
-    local FILE_PATH=$HOME/Downloads/postman.tar.gz
     local DIR_WORKSPACE=$HOME/tools
-    wget -O $FILE_PATH https://dl.pstmn.io/download/latest/linux64
+    wget -O $HOME/Downloads/postman.tar.gz https://dl.pstmn.io/download/latest/linux64
     if [ ! -d $DIR_WORKSPACE ]; then
             mkdir $DIR_WORKSPACE
     fi 
-    tar -zxvf $FILE_PATH -C $DIR_WORKSPACE
-    rm $FILE_PATH
+    tar -zxvf $HOME/Downloads/postman.tar.gz -C $DIR_WORKSPACE
+    rm $HOME/Downloads/postman.tar.gz
+}
+
+install_toolbox() {
+    wget -O $HOME/Downloads/toolbox.tar.gz https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.16.6319.tar.gz
+    tar -xzf $HOME/Downloads/toolbox.tar.gz -C $HOME/tools
+    rm $HOME/Downloads/toolbox.tar.gz
 }
 
 donwload_icons() {
-
-    local DIR_WORKSPACE=$HOME/workspace/icons
-    if [ ! -d $DIR_WORKSPACE ]; then
-            mkdir $DIR_WORKSPACE
+    local DIR_TOOLS=$HOME/tools/icons
+    if [ ! -d $DIR_TOOLS ]; then
+            mkdir $DIR_TOOLS
     fi 
-    wget -O $DIR_WORKSPACE"/VScode.png" https://klauslaube.com.br/images/blog/vscode-logo.png
-    wget -O $DIR_WORKSPACE"/postman.png" https://miro.medium.com/max/630/1*fVBL9mtLJmHIH6YpU7WvHQ.png
+    wget -O $DIR_TOOLS"/postman.png" https://miro.medium.com/max/630/1*fVBL9mtLJmHIH6YpU7WvHQ.png
 }
 
 config_keyboard() {
@@ -119,6 +113,24 @@ config_keyboard() {
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['']"
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['']"
+}
+
+install_zsh() {
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+    cp ./files/zshrc ~/.zshrc
+}
+
+install_dracula_theme() {
+    git clone https://github.com/dracula/gnome-terminal
+    local DIR_TOOLS=$HOME/workspace
+    if [ ! -d $DIR_TOOLS ]; then
+            mkdir $DIR_TOOLS
+    fi 
+    mv gnome-terminal $HOME/workspace
+    sudo bash $HOME/workspace/gnome-terminal/install.sh -y
 }
 
 run() {
@@ -129,19 +141,23 @@ run() {
     sleep 8s
 
     system_update
+    install_zsh
+    install_dracula_theme
+    install_toolbox
     install_chrome
     install_slack
     install_gnome
     install_git
+    install_jdk13
+    install_jdk12
     install_jdk11
+    install_jdk8
     install_node
     install_vs_code
-    install_intellij
     install_spotify
     install_docker
     install_pulse
     install_gradle
-    install_jdk8
     install_postman
     install_npm
     config_keyboard
